325c9d55f1799d7e7f18459866a703c20fd84447
DayneTes <dt00179@my.westga.edu>
2025-10-28 22:58:21 -0400

    th

--- src/pages/Programs/OperationHolidayAtHome/Donors.tsx
+++ src/pages/Programs/OperationHolidayAtHome/Donors.tsx
@@ -4,6 +4,7 @@ import ProductCard from "../../../components/ProductCard";
 import Accordion from "../../../components/Accordion";
 //import FamilyDetail from "./FamilyDetail";
 import FamilyCard from "../../../components/FamilyCard";
+import headerImg from "../../../assets/images/applicant-imgs/trees-bg-header-applicants.webp"
 
 export default function Donors() {
   // Example placeholder data
@@ -54,7 +55,7 @@ export default function Donors() {
           className="text-white flex items-center justify-center"
           style={{
             background: `linear-gradient(rgba(0,0,0,0.6), rgba(0,0,0,0.6)),
-              url('/trees-bg-header-applicants.webp')`,
+              url(${headerImg})`,
             backgroundSize: "cover",
             backgroundPosition: "center",
             height: "20vh",
